b22dc766f71c8b0b0fd755a481e5cc56148ba7b3
Manish Verma <vermam318@gmail.com>
2021-05-05 16:35:15 +0530

    data added using json and js file

---
+++ students.js
@@ -0,0 +1,4 @@
+db.students.insertOne({name: "Durga", rollno: 101, marks: 90})
+db.students.insertOne({name: "Ravi", rollno: 102, marks: 92})
+db.students.insertOne({name: "Devesh", rollno: 103, marks: 94})
+db.students.insertOne({name: "Rishi", rollno: 104, marks: 93})
